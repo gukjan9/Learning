731c8765c83ce25d668d7f5158d4739d63017202
NASA-GukJang <gukjang1997@gmail.com>
2021-02-02 00:49:20 +0900

    상수 형태 문자열

---
+++ C/C_Programming-YoonSungWoo/p_302.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    char str1[] = "My String";
+    char *str2 = "Your String";
+    printf("%s %s\n", str1, str2);
+
+    str2 = "Our String";
+    printf("%s %s\n", str1, str2);
+
+    str1[0] = 'X';
+    // str2[0] = 'X'; 실행 안됨
+    printf("%s %s\n", str1, str2);
+
+    return 0;
+}
