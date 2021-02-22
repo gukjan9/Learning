5fe52e6266f258ee3d5adae58e02a1ed98a14b46
NASA-GukJang <gukjang1997@gmail.com>
2021-02-23 02:52:06 +0900

    Pointer Swap Function

---
+++ C/C_Programming-YoonSungWoo/p_363.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void SwapIntPtr(int **, int **);
+
+int main(void){
+    int num1 = 10, num2 = 20;
+    int *ptr1, *ptr2;
+    ptr1 = &num1, ptr2 = &num2;
+
+    printf("*ptr1, *ptr2 : %d %d \n", *ptr1, *ptr2);
+
+    SwapIntPtr(&ptr1, &ptr2); // ptr1과 ptr2의 주소값 전달
+    printf("*ptr1, *ptr2 : %d %d \n", *ptr1, *ptr2);
+    return 0;
+}
+
+void SwapIntPtr(int **dp1, int **dp2){
+    int *temp = *dp1;
+    *dp1 = *dp2;
+    *dp2 = temp;
+}
