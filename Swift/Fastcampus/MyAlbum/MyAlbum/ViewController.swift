3ca0bd7247bb1f6e68e4fc7a4d255b3b40e870f2
NASA-GukJang <gukjang1997@gmail.com>
2021-04-02 17:06:48 +0900

    button and alert

---
+++ Swift/Fastcampus/MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by GukJang on 2021/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
