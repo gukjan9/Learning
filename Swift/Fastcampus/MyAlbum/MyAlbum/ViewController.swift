093625b0af4c45eb2a4ef3c8948e17f770c7ffd0
NASA-GukJang <gukjang1997@gmail.com>
2021-04-04 17:08:15 +0900

    constraints, app icon, method

--- Swift/Fastcampus/MyAlbum/MyAlbum/ViewController.swift
+++ Swift/Fastcampus/MyAlbum/MyAlbum/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0        // option 누르고 ? 클릭하면 타입형 나옴
 
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refresh()
     }
-
-
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 ₩\(currentValue) 입니다"
+        
+        let alert = UIAlertController(title : "Hello",
+                                      message : message,
+                                      preferredStyle : .alert)
+        let action = UIAlertAction(title : "OK",
+                                   style : .default,
+                                   handler : { action in        // closure
+                                    self.refresh()
+                                   })
+        alert.addAction(action)
+        present(alert, animated : true, completion : nil)
+    }
+    
+    func refresh(){         // method
+        let randomPrice = arc4random_uniform(10000) + 1     // 0 ~ 9999 까지 수 리턴하므로 +1 해주기
+        currentValue = Int(randomPrice)
+        priceLabel.text = "₩ \(currentValue)"
+    }
 }
+
+// appicon generator
 
