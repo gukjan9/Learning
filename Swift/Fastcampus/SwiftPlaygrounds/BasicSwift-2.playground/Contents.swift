8480af103a3a94f97028f9509633f7706504fbb1
NASA-GukJang <gukjang1997@gmail.com>
2021-05-12 00:13:35 +0900

    switch문

--- Swift/Fastcampus/SwiftPlaygrounds/BasicSwift-2.playground/Contents.swift
+++ Swift/Fastcampus/SwiftPlaygrounds/BasicSwift-2.playground/Contents.swift
@@ -81,3 +81,63 @@ for i in closedRange {
     }
 }
 
+
+// Switch
+let num = 10
+
+switch num {
+case 0 :
+    print("--> 0 입니다. ")
+case 0..<10 :
+    print("--> 0 에서 10 사이 입니다. ")
+case 10:
+    print("--> 10 입니다. ")
+default :
+    print("--> 나머지 입니다. ")
+}
+
+
+let pet = "bird"
+
+switch pet {
+case "dog", "cat", "bird":
+    print("--> It's a animal. ")
+default:
+    print("--> I don't know. ")
+}
+
+
+let num2 = 20
+switch num {
+case _ where num % 2 == 0:
+    print("--> even ")
+default :
+    print("--> odd ")
+}
+
+
+let coordinate = (x : 0, y : 10)
+switch coordinate{
+case (0, 0):
+    print("--> 원점입니다. ")
+case (_, 0):
+    print("--> x축 입니다. ")
+case (0, _):
+    print("--> y축 입니다. ")
+default:
+    print("Unknown")
+}
+
+let coordinate2 = (x : 10, y : 10)
+switch coordinate2{
+case (0, 0):
+    print("--> 원점입니다. ")
+case (let x, 0):
+    print("--> x축 입니다, x:\(x)")
+case (0, let y):
+    print("--> y축 입니다, y:\(y)")
+case (let x, let y) where x == y:
+    print("x랑 y랑 같음, x, y = \(x),\(y)")
+case (let x, let y):
+    print("Unknown, x, y = \(x),\(y)")
+}
