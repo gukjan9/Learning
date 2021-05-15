575ad385e294a65659392fc8833f2f629c863766
NASA-GukJang <gukjang1997@gmail.com>
2021-05-15 23:29:04 +0900

    Set

---
+++ Swift/Fastcampus/SwiftPlaygrounds/BasicSwift-11.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Set - Array 와 비슷하지만 중북이 없다
+var someArray: Array<Int> = [1, 2, 3, 1]
+var someSet: Set<Int> = [1, 2, 3, 1]
+
+someSet.isEmpty
+someSet.count
+
+someSet.contains(4)
+someSet.contains(1)
+
+someSet.insert(5)
+someSet
+
+someSet.remove(1)
+someSet
