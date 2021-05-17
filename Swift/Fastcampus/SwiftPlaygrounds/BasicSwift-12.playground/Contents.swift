99fb01739cda496116814bc283a895498e1851c3
NASA-GukJang <gukjang1997@gmail.com>
2021-05-17 22:57:34 +0900

    Closure

---
+++ Swift/Fastcampus/SwiftPlaygrounds/BasicSwift-12.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// Closure - 이름이 없는 Methods
+//var multiplyClosure: (Int, Int) -> Int = { a, b in
+//    return a * b
+//}
+
+var multiplyClosure: (Int, Int) -> Int = { $0 * $1 }
+
+let result = multiplyClosure(4, 2)
+
+
+func operateTwoNum(a: Int, b: Int, operation: (Int, Int) -> Int) -> Int {
+    let result = operation(a, b)
+    return result
+}
+
+operateTwoNum(a: 4, b: 2, operation: multiplyClosure)
+
+var addClosure: (Int, Int) -> Int = {a, b in
+    return a + b
+}
+operateTwoNum(a: 4, b: 2, operation: addClosure)
+
+
+operateTwoNum(a: 4, b: 2){ a, b in
+    return a / b
+}
