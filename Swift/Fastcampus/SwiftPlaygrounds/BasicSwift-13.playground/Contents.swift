370e5f423887b9e1ad96bc362bd8237549bc96da
NASA-GukJang <gukjang1997@gmail.com>
2021-05-19 00:14:43 +0900

    Closure 개념

---
+++ Swift/Fastcampus/SwiftPlaygrounds/BasicSwift-13.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+/* Closure
+함수는 Closure 의 한 종류
+함수처럼 기능을 수행하는 코드블록, 이름은 없다.
+
+Closure - Global 함수
+          Nested 함수
+          Closure Expressions */
+
+/* First Class Type
+ 1. 변수에 할당할 수 있다.
+ 2. 인자로 받을 수 있다.
+ 3. 리턴 할 수 있다.
+*/
+
+
+// Completion Block
+
+// Higher Order Functions - 인자를 함수로 받을 수 있는 함수
