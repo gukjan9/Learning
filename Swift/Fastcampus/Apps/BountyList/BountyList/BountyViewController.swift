77535c176492cab31dffa32b95638336f5cc94f1
NASA-GukJang <gukjang1997@gmail.com>
2021-06-15 22:47:29 +0900

    Make list of bounty

--- Swift/Fastcampus/Apps/BountyList/BountyList/BountyViewController.swift
+++ Swift/Fastcampus/Apps/BountyList/BountyList/BountyViewController.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
-
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 1200000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else{
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+        return cell
+
+        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+//            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//            cell.imgView.image = img
+//            cell.nameLabel.text = nameList[indexPath.row]
+//            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+//            return cell
+//        } else {
+//            return UITableViewCell()
+//        }
+    }
+    
+    // UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("--> \(indexPath.row)")
+    }
+    
+    
+
+    class ListCell: UITableViewCell {
+        @IBOutlet weak var imgView: UIImageView!
+        @IBOutlet weak var nameLabel: UILabel!
+        @IBOutlet weak var bountyLabel: UILabel!
+    }
 
 }
