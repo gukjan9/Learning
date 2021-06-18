c9ce0c18ee2627ff437d30c0dd603b9469116d4e
NASA-GukJang <gukjang1997@gmail.com>
2021-06-19 03:23:01 +0900

    How to use scanner in Java

---
+++ Java/JavaJeongSeok-NamGungSeong/p_40.java
@@ -0,0 +1,16 @@
+/* Scanner */
+import java.util.*;
+
+public class p_40 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);       // Scanner 클래스의 객체 생성
+
+        String input = scanner.nextLine();      // 입력 받기
+        int num = Integer.parseInt(input);      // 입력 받은 문자열을 숫자로 변환
+
+        System.out.println("What I wrote : " + input);
+        System.out.printf("num = %d%n", num);
+
+        scanner.close();
+    }    
+}
