fc314e79660d3e4cbd4849a277ee8c3117e94cbc
NASA-GukJang <gukjang1997@gmail.com>
2021-06-23 23:57:28 +0900

    Type Casting

--- Java/JavaJeongSeok-NamGungSeong/p_78.java
+++ Java/JavaJeongSeok-NamGungSeong/p_78.java
@@ -1,11 +1,11 @@
 public class p_78 {
     public static void main(String[] args){
         float f = 9.1234567f;
-        double d = 9.1234567;
+        double d = 9.1234567;           // double 은 f 안 붙여줘도 된다.
         double d2 = (double)f;
 
         System.out.printf("f = %20.18f\n", f);
         System.out.printf("d = %20.18f\n", d);
-        System.out.printf("d2 = %20.18f\n", d2);
+        System.out.printf("d2 = %20.18f\n", d2);        
     }
 }
