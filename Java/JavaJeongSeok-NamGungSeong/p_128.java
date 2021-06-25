b1d878789690bb25c44ec87fc34e0df8db948554
NASA-GukJang <gukjang1997@gmail.com>
2021-06-25 21:19:48 +0900

    16진수 한자리씩 추출 (shift, and operator)

---
+++ Java/JavaJeongSeok-NamGungSeong/p_128.java
@@ -0,0 +1,21 @@
+// Shift 와 And Operator 를 이용하여 16진수를 끝에서부터 한자리씩 뽑아낸다.
+
+public class p_128 {
+    public static void main(String[] args){
+        int dec = 1234;
+        int hex = 0xABCD;
+        int mask = 0xF;
+
+        System.out.printf("hex = %X%n", hex);
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+    }
+}
