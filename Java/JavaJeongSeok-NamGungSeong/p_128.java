d487c34b55b02ac5adbaac3dac146a9dca3c59b2
NASA-GukJang <gukjang1997@gmail.com>
2021-06-26 21:12:13 +0900

    shift 와 and operator 를 이용한 16진수 한자리씩 추출

--- Java/JavaJeongSeok-NamGungSeong/p_128.java
+++ Java/JavaJeongSeok-NamGungSeong/p_128.java
@@ -7,15 +7,15 @@ public class p_128 {
         int mask = 0xF;
 
         System.out.printf("hex = %X%n", hex);
-        System.out.printf("%X%n", hex & mask);
+        System.out.printf("%X%n", hex & mask);      // D
 
         hex = hex >> 4;
-        System.out.printf("%X%n", hex & mask);
+        System.out.printf("%X%n", hex & mask);      // C
 
         hex = hex >> 4;
-        System.out.printf("%X%n", hex & mask);
+        System.out.printf("%X%n", hex & mask);      // B
 
         hex = hex >> 4;
-        System.out.printf("%X%n", hex & mask);
+        System.out.printf("%X%n", hex & mask);      // A
     }
 }
