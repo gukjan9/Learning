cfb9ce68946bda0018582c14b8bc2e2795d193ef
NASA-GukJang <gukjang1997@gmail.com>
2021-06-28 16:06:22 +0900

    Math.random() 범위 설정

---
+++ Java/JavaJeongSeok-NamGungSeong/p_150.java
@@ -0,0 +1,10 @@
+public class p_150 {
+    public static void main(String[] args){
+        int com;
+
+        for (int i = 0; i < 5; i++) {
+            com = (int) (Math.random() * 3) + 1;
+            System.out.println(com);
+        }
+    }
+}
