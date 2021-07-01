4e23ba4bc15c1e5e03b7a47be0d62c3e54bbeea0
NASA-GukJang <gukjang1997@gmail.com>
2021-07-02 01:54:18 +0900

    arraycopy

---
+++ Java/JavaJeongSeok-NamGungSeong/p_195.java
@@ -0,0 +1,15 @@
+public class p_195 {
+    public static void main(String[] args){
+        char[] abc = {'A', 'B'};
+        char[] num = {'0', '1', '2', '3', '4'};
+
+        char[] result = new char[abc.length+num.length];
+
+        // System.arraycopy(num, 0, newNum, 0, num.length);
+        // num[0] -> newNum[0] num.length 만큼 복사
+        System.arraycopy(abc, 0, result, 0, abc.length);
+        System.arraycopy(num, 0, result, abc.length, num.length);
+
+        System.out.println(result);
+    }
+}
