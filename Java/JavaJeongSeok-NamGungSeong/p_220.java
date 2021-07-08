e5f3efb4d88733560aa87f0c886c47ccaee48574
NASA-GukJang <gukjang1997@gmail.com>
2021-07-08 23:53:16 +0900

    MultiArr

---
+++ Java/JavaJeongSeok-NamGungSeong/p_220.java
@@ -0,0 +1,12 @@
+public class p_220 {
+    public static void main(String[] args){
+        int[][] score = {
+            {100, 100, 100, 100},
+            {20, 20, 20},
+            {30, 30},
+            {40, 40},
+            {50, 50, 50}
+        };
+        
+    }
+}
