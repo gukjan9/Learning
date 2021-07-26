ec19314e35ed2167ec725545a7bbceff49ee3fdc
NASA-GukJang <gukjang1997@gmail.com>
2021-07-26 23:41:08 +0900

    regular expression 정규식

---
+++ Java/JavaJeongSeok-NamGungSeong/p_505.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;
+
+public class p_505 {
+    public static void main(String[] args){
+        String[] data = {"bat", "baby", "bonus", "cA", "ca", "co", "c."};
+
+        Pattern p = Pattern.compile("c[a-z]*");
+
+        for(int i = 0; i < data.length; i++){
+            Matcher m = p.matcher(data[i]);
+            if(m.matches()){
+                System.out.print(data[i] + ",");
+            }
+        }
+    }
+}
