110870092365821c859195b7e4754845eac28c9b
NASA-GukJang <gukjang1997@gmail.com>
2021-07-27 23:04:28 +0900

    StringTokenizer

--- Java/JavaJeongSeok-NamGungSeong/p_505.java
+++ Java/JavaJeongSeok-NamGungSeong/p_505.java
@@ -4,7 +4,7 @@ public class p_505 {
     public static void main(String[] args){
         String[] data = {"bat", "baby", "bonus", "cA", "ca", "co", "c."};
 
-        Pattern p = Pattern.compile("c[a-z]*");
+        Pattern p = Pattern.compile("c[a-z]*");         // c로 시작하는 알파벳 단어
 
         for(int i = 0; i < data.length; i++){
             Matcher m = p.matcher(data[i]);
