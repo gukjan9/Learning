0c79f53f7dfa8fc6f9fa9d259f182138e7e1d81b
NASA-GukJang <gukjang1997@gmail.com>
2021-07-28 23:24:48 +0900

    StringTokenizer

--- Java/JavaJeongSeok-NamGungSeong/p_514.java
+++ Java/JavaJeongSeok-NamGungSeong/p_514.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class p_514 {
     public static void main(String[] args){
         String source = "100,200,300,400,";
-        StringTokenizer st = new StringTokenizer(source, ",");
+        StringTokenizer st = new StringTokenizer(source, ",");          // , 기준으로 문자열 자르기
 
         while(st.hasMoreTokens()){
             System.out.println(st.nextToken());
