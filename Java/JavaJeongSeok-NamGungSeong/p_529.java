b4be34ffe57fe2f3d8f75c90472819c4ec0b757a
NASA-GukJang <gukjang1997@gmail.com>
2021-07-31 02:45:20 +0900

    Calendar

---
+++ Java/JavaJeongSeok-NamGungSeong/p_529.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class p_529 {
+    public static void main(String[] args){
+        Calendar today = Calendar.getInstance();
+        System.out.println("Year : " + today.get(Calendar.YEAR));
+        // WEEK_OF_YEAR WEEK_OF_MONTH
+    }
+}
