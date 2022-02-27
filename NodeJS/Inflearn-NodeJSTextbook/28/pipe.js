d47ace035381e895f3a5ec9e3a5ee02ee24bd034
GukJan9 <gukjang1997@gmail.com>
2022-02-28 03:17:08 +0900

    zlib 압축 스트림

--- NodeJS/Inflearn-NodeJSTextbook/28/pipe.js
+++ NodeJS/Inflearn-NodeJSTextbook/28/pipe.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const zlib = require('zlib'); // 압축
 
 const readStream = fs.createReadStream('../27/readme.txt', {highWaterMark: 16});
-const writeStream = fs.createWriteStream('./writeme.txt');
+const zlibStream = zlib.createGzip();
+// const writeStream = fs.createWriteStream('./writeme.txt');
+const writeStream = fs.createWriteStream('./writeme.txt.gz');
 
 readStream.pipe(writeStream);
