b9024fcb83a5bbe918fd8a61b123ba291828c1fb
GukJan9 <gukjang1997@gmail.com>
2022-03-12 23:02:34 +0900

    next, error 처리

--- NodeJS/Inflearn-NodeJSTextbook/43/app.js
+++ NodeJS/Inflearn-NodeJSTextbook/43/app.js
@@ -12,10 +12,21 @@ app.set('port', 3000);
 
 // 미들웨어
 // 사용 후, next 선언해줘야 다음 라우터로 넘어간다.
+// ((req, res)) 부터 }); 까지가 미들웨어
 app.use((req, res, next) => {
     console.log('모든 요청에 실행하고 싶어요.');
     next();
+}, (req, res, next) => {
+    // throw new Error('에러가 났어요.');      // 이렇게 하면 서버로그에 떠야할 에러메세지가 웹페이지에 나타나게됨
+    try{
+        console.log('error');
+    } catch(error){
+        next(error);        // next 에 에러 넣으면 에러 처리 미들웨어로 이동
+    }
 });
+
+// next
+// next('route') -> 다음 라우터로 이동
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));       // 위에 path 에서 경로 지정
@@ -40,8 +51,19 @@ app.get('/category/:name', (req, res) => {
 });
 
 // * asterisk - 모든 주소 처리
-app.get('*', (req, res) => {
-    res.send(`hello ${req.params.name}`);
+// app.get('*', (req, res) => {
+//     res.send(`hello ${req.params.name}`);
+// });
+
+app.use((req, res, next) => {
+    res.send('404');
+});
+
+// error 미들웨어를 가장 마지막에
+// 4개의 파라미터 필
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(200).send('Error occured');
 });
 
 app.listen(app.get('port'), () => {                // app.set 3000
