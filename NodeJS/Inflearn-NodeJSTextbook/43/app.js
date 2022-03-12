27aabfe9e031a95b20c748dc57904a4fc9bd3f83
GukJan9 <gukjang1997@gmail.com>
2022-03-12 19:29:14 +0900

    path 지정으로 파일 불러오기

--- NodeJS/Inflearn-NodeJSTextbook/43/app.js
+++ NodeJS/Inflearn-NodeJSTextbook/43/app.js
@@ -1,5 +1,6 @@
-const { application } = require('express');
+// const { application } = require('express');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.set('port', 3000);
 // app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.send('hello express');
+    res.sendFile(path.join(__dirname, 'index.html'));       // 위에 path 에서 경로 지정
 });
 
 app.get('/about', (req, res) => {
