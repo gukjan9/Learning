c74f9c7517fa0bd3add52bad56de27b1d8956105
GukJang <gukjang1997@gmail.com>
2022-06-14 19:44:20 +0900

    채팅방 구조 잡기

--- NodeJS/Inflearn-NodeJSTextbook/98/app.js
+++ NodeJS/Inflearn-NodeJSTextbook/98/app.js
@@ -5,10 +5,12 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const ColorHash = require('color-hash');
 
 dotenv.config();
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
 app.set('port', process.env.PORT || 8005);
@@ -17,6 +19,7 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+connect();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,6 +35,14 @@ app.use(session({
     secure: false,
   },
 }));
+
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
 
 app.use('/', indexRouter);
 
@@ -52,4 +63,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server);
+webSocket(server, app);
