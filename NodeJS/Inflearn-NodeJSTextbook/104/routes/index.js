bd24d7fcd47919253241006eb6df43f664550773
GukJang <gukjang1997@gmail.com>
2022-06-19 23:46:46 +0900

    실시간 경매 시스템

--- NodeJS/Inflearn-NodeJSTextbook/104/routes/index.js
+++ NodeJS/Inflearn-NodeJSTextbook/104/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const schedule = require('node-schedule');
 
 const { Good, Auction, User } = require('../models');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
@@ -57,16 +58,94 @@ const upload = multer({
 router.post('/good', isLoggedIn, upload.single('img'), async (req, res, next) => {
   try {
     const { name, price } = req.body;
-    await Good.create({
+    const good = await Good.create({
       OwnerId: req.user.id,
       name,
       img: req.file.filename,
       price,
     });
+    const end = new Date();
+    end.setDate(end.getDate() + 1); // 하루 뒤
+    schedule.scheduleJob(end, async () => {
+      const success = await Auction.findOne({
+        where: { GoodId: good.id },
+        order: [['bid', 'DESC']],
+      });
+      await Good.update({ SoldId: success.UserId }, { where: { id: good.id } });
+      await User.update({
+        money: sequelize.literal(`money - ${success.bid}`),
+      }, {
+        where: { id: success.UserId },
+      });
+    });
     res.redirect('/');
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+// 입찰 화면
+router.get('/good/:id', isLoggedIn, async (req, res, next) => {
+  try {
+    const [good, auction] = await Promise.all([
+      Good.findOne({
+        where: { id: req.params.id },
+        include: {
+          model: User,
+          as: 'Owner',
+        },
+      }),
+      Auction.findAll({
+        where: { goodId: req.params.id },
+        include: { model: User },
+        order: [['bid', 'ASC']],
+      }),
+    ]);
+    res.render('auction', {
+      title: `${good.name} - NodeAuction`,
+      good,
+      auction,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/good/:id/bid', isLoggedIn, async (req, res, next) => {
+  try {
+    const { bid, msg } = req.body;
+    const good = await Good.findOne({
+      where: { id: req.params.id },
+      include: { model: Auction },
+      order: [[{ model: Auction }, 'bid', 'DESC']],
+    });
+    if (good.price >= bid) {
+      return res.status(403).send('시작 가격보다 높게 입찰해야 합니다.');
+    }
+    if (new Date(good.createdAt).valueOf() + (24 * 60 * 60 * 1000) < new Date()) {
+      return res.status(403).send('경매가 이미 종료되었습니다');
+    }
+    if (good.Auctions[0] && good.Auctions[0].bid >= bid) {
+      return res.status(403).send('이전 입찰가보다 높아야 합니다');
+    }
+    const result = await Auction.create({
+      bid,
+      msg,
+      UserId: req.user.id,
+      GoodId: req.params.id,
+    });
+    // 실시간으로 입찰 내역 전송
+    req.app.get('io').to(req.params.id).emit('bid', {
+      bid: result.bid,
+      msg: result.msg,
+      nick: req.user.nick,
+    });
+    return res.send('ok');
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 
