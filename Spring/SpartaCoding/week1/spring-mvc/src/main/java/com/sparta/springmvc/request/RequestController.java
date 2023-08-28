b5c95f40121e3e4d1f1a1c71a90e14c05fd7296a
GukJang <9ukjang@gmail.com>
2023-08-28 14:28:53 +0900

    Path Variable, Request Param

--- Spring/SpartaCoding/week1/spring-mvc/src/main/java/com/sparta/springmvc/request/RequestController.java
+++ Spring/SpartaCoding/week1/spring-mvc/src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -1,10 +1,7 @@
 package com.sparta.springmvc.request;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello/request")
@@ -18,8 +15,29 @@ public class RequestController {
     // GET http://localhost:8080/hello/request/star/Robbie/age/95
     @GetMapping("/star/{name}/age/{age}")
     @ResponseBody
-    public String helloRequestPath(@PathVariable String name, @PathVariable int age)
-    {
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
         return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // request param, query string
+    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    // (required = false) :
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloGetRequestParam(@RequestParam(required = false) String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/param
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/param")
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
 }
