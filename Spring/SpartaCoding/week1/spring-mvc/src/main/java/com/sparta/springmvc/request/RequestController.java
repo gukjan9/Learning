aa23d36e372aeadcd17a5c7b3745c0065d10de5b
GukJang <9ukjang@gmail.com>
2023-08-28 15:40:25 +0900

    http 데이터를 객체로 처리하는 방법

--- Spring/SpartaCoding/week1/spring-mvc/src/main/java/com/sparta/springmvc/request/RequestController.java
+++ Spring/SpartaCoding/week1/spring-mvc/src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -22,7 +22,8 @@ public class RequestController {
     // [Request sample]
     // request param, query string
     // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
-    // (required = false) :
+    // (required = false) : Param 값 전달 안해도 에러 안 생김
+    // requestParam 생략가능
     @GetMapping("/form/param")
     @ResponseBody
     public String helloGetRequestParam(@RequestParam(required = false) String name, @RequestParam int age) {
@@ -34,10 +35,45 @@ public class RequestController {
     // Header
     //  Content type: application/x-www-form-urlencoded
     // Body
-    //  name=Robbie&age=95
+    //  name=Robbie&age=95 (query string 방식)
     @PostMapping("/form/param")
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    // @ModelAttribute 객체에 매핑
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    // query String or request param
+    // 객체 사용하려면 setter or 오버로딩된 생성자 필요
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
     }
 }
---
+++ Spring/SpartaCoding/week1/spring-mvc/src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,11 @@
+package com.sparta.springmvc.request;
+
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
