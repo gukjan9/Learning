f05d2bf2c8975c88038324f0bd78c628fc919850
GukJang <9ukjang@gmail.com>
2023-08-29 17:52:58 +0900

    Entity 이해하기

---
+++ Spring/SpartaCoding/week1/jpa-core/src/main/java/com/sparta/entity/Memo.java
@@ -0,0 +1,44 @@
+package com.sparta.entity;
+
+import jakarta.persistence.*;
+
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+public class Memo {
+    @Id     // PK
+    // @GeneratedValue(strategy = GenerationType.IDENTITY) - Test 할 때 ID 값을 직접 넣기 때문에 오류 발생
+    private Long id;
+
+    // nullable: null 허용 여부
+    // unique: 중복 허용 여부 (false 일때 중복 허용)
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    // length: 컬럼 길이 지정
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
