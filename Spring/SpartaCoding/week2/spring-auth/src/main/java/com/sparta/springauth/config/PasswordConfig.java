4295f80e3b54f8699f0e89fbca62d5bedfe2c4c0
GukJang <9ukjang@gmail.com>
2023-08-30 16:50:31 +0900

    Bean 을 수동으로 등록하는 방법

---
+++ Spring/SpartaCoding/week2/spring-auth/src/main/java/com/sparta/springauth/config/PasswordConfig.java
@@ -0,0 +1,16 @@
+package com.sparta.springauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt - Hash함수
+        return new BCryptPasswordEncoder();
+    }
+}
