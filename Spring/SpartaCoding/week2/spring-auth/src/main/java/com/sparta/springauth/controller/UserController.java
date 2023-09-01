e88619fe05909c8e4a962e302c6ec9736a2a66b3
GukJang <gukjang1997@gmail.com>
2023-09-01 16:49:53 +0900

    Validation 예외처리

--- Spring/SpartaCoding/week2/spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ Spring/SpartaCoding/week2/spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -4,18 +4,25 @@ import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// controller 여서 html 자동 찾아짐
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
+
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -30,21 +37,18 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto){
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
     }
-
-//      로그인은 필터 단에서 처리
-//    @PostMapping("/user/login")
-//    public String login(LoginRequestDto requestDto, HttpServletResponse res){
-//        try {
-//            userService.login(requestDto, res);
-//        } catch (Exception e) {
-//            return "redirect:/api/user/login-page?error";
-//        }
-//
-//        return "redirect:/";
-//    }
 }
