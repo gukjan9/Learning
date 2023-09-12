f08b6fac8af6f0d8de66bad391d12bf1432f4c81
GukJang <gukjang1997@gmail.com>
2023-09-12 22:23:14 +0900

    init

---
+++ Spring/SpartaCoding/week2/junit5-practice/src/main/java/com/sparta/junit5practice/Junit5PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.junit5practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5PracticeApplication.class, args);
+	}
+
+}
