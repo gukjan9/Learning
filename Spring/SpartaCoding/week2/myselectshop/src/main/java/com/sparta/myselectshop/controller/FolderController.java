62a889ae5f1f9170e662f9b45e775d81ef729a18
GukJang <gukjang1997@gmail.com>
2023-09-13 19:19:57 +0900

    @ControllerAdvice 적용

--- Spring/SpartaCoding/week2/myselectshop/src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ Spring/SpartaCoding/week2/myselectshop/src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -29,16 +29,4 @@ public class FolderController {
     public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails){
         return folderService.getFolders(userDetails.getUser());
     }
-
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
-        System.out.println("FolderController.handleException");
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.BAD_REQUEST
-        );
-    }
 }
---
+++ Spring/SpartaCoding/week2/myselectshop/src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// 공통적으로 예외 처리하기 위한 annotation
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
