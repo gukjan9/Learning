7d673959973cc4819ff5c56bb8c955b9beba4524
GukJang <9ukjang@gmail.com>
2023-09-13 14:14:18 +0900

    controller 테스트

--- Spring/SpartaCoding/week2/myselectshop/src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ Spring/SpartaCoding/week2/myselectshop/src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class MyselectshopApplication {
 
