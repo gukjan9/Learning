d110a802d9f531f508d1de0dbd3d09e8d5f29fa8
GukJang <9ukjang@gmail.com>
2024-04-04 16:28:38 +0900

    init

---
+++ Spring/FastCampus-9ProjectsHandlingMassiveTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.boardserver.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@Log4j2
+public class UserController {
+
+}
