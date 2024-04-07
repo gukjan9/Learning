f3b8a47a1dbbdb7bdbee391a4373045b39c81ca9
GukJang <9ukjang@gmail.com>
2024-04-07 22:17:50 +0900

    [catch up]

---
+++ Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/config/MysqlConfig.java
@@ -0,0 +1,20 @@
+package com.fastcampus.boardserver.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.fastcampus.boardserver.mapper")
+public class MysqlConfig {
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+    }
+}
