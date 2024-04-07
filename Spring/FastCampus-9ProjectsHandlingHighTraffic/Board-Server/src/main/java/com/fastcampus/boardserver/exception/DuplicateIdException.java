4b024532fcedfcab10ea0f98179490af06cb9f59
GukJang <9ukjang@gmail.com>
2024-04-07 23:46:44 +0900

    feat: 유저 회원가입 구현
    - 아이디 중복 체크

---
+++ Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/exception/DuplicateIdException.java
@@ -0,0 +1,7 @@
+package com.fastcampus.boardserver.exception;
+
+public class DuplicateIdException extends RuntimeException {
+    public DuplicateIdException(String msg) {
+        super(msg);
+    }
+}
--- Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
+++ Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
@@ -1,14 +1,40 @@
 package com.fastcampus.boardserver.service.impl;
 
 import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.exception.DuplicateIdException;
+import com.fastcampus.boardserver.mapper.UserProfileMapper;
 import com.fastcampus.boardserver.service.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
+    @Autowired
+    public UserProfileMapper userProfileMapper;
+
+    public UserServiceImpl(UserProfileMapper userProfileMapper){
+        this.userProfileMapper = userProfileMapper;
+    }
+
     @Override
     public void register(UserDTO userProfile) {
+        boolean duplIdResult = isDuplicatedId(userProfile.getUserId());
+        if(duplIdResult){
+            throw new DuplicateIdException("중복된 아이디 입니다.");
+        }
+        userProfile.setCreateTime(new Date());
+        userProfile.setPassword(encryptSHA(userProfile.getPassword()));
+        int insertCount = userProfileMapper.insertUserProfile(userProfile);
 
+        if(insertCount != 1){
+            log.error("insertMember ERROR! {}", userProfile);
+            throw new RuntimeException(
+                    "insertUser ERROR! 회원가입 메서드를 확인해주세요\n" + "Params : " + userProfile);
+        }
     }
 
     @Override
@@ -18,7 +44,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean isDuplicatedId(String id) {
-        return false;
+        return userProfileMapper.idCheck(id) == 1;
     }
 
     @Override
