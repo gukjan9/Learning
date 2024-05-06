d4cf9379b23f09eed9bc2ec5cf25531f651dbcf2
GukJang <9ukjang@gmail.com>
2024-05-06 20:33:42 +0900

    feat: 카테고리 crud 구현

--- Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/service/impl/CategoryServiceImpl.java
+++ Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server/src/main/java/com/fastcampus/boardserver/service/impl/CategoryServiceImpl.java
@@ -15,16 +15,34 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void register(String accountId, CategoryDTO categoryDTO) {
-
+        if(accountId != null){
+            categoryMapper.register(categoryDTO);
+        }
+        else {
+            log.error("resister ERROR! {}", categoryDTO);
+            throw new RuntimeException("register Error! 게시글 카테고리 등록 메서드를 확인해주세요" + categoryDTO);
+        }
     }
 
     @Override
     public void update(CategoryDTO categoryDTO) {
-
+        if(categoryDTO != null){
+            categoryMapper.updateCategory(categoryDTO);
+        }
+        else {
+            log.error("update ERROR! {}", categoryDTO);
+            throw new RuntimeException("update Error! 게시글 카테고리 수정 메서드를 확인해주세요" + categoryDTO);
+        }
     }
 
     @Override
     public void delete(int categoryId) {
-
+        if(categoryId != 0){
+            categoryMapper.deleteCategory(categoryId);
+        }
+        else {
+            log.error("delete ERROR! {}", categoryId);
+            throw new RuntimeException("delete Error! 게시글 카테고리 삭제 메서드를 확인해주세요" + categoryId);
+        }
     }
 }
