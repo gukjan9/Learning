4f11f92e0ade6d4793e76be7aaccc6d09b0cba3b
GukJang <9ukjang@gmail.com>
2024-05-17 20:55:28 +0900

    feat: 게시글 작성 locust 성능 테스트

---
+++ Spring/FastCampus-9ProjectsHandlingHighTraffic/Board-Server-Locust/venv/AddPosts.py
@@ -0,0 +1,18 @@
+from locust import HttpUser, task, between
+import random
+
+class AddPosts(HttpUser):
+    waite_time = between(1, 2)  # 잠시 여유를 줌
+
+    def on_start(self):
+        self.client.post("/users/sign-in", json = {"userId": "topojs9",
+                                                   "password": "123"})
+
+    @task
+    def add_post(self):
+        self.client.post("/posts", json = {
+            "name": "테스트 게시글" + str(random.randint(1, 100000)),
+            "contents": "테스트 컨텐츠" + str(random.randint(1, 100000)),
+            "categoryId": random.randint(1, 10),
+            "fileId": random.randint(1, 10)
+        })
