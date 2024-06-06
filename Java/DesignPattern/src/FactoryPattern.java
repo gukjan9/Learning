bb4088fe060ab52cc8219178a0f0136565f3782f
GukJang <9ukjang@gmail.com>
2024-06-06 18:33:32 +0900

    Factory 패턴

---
+++ Java/DesignPattern/src/FactoryPattern.java
@@ -0,0 +1,44 @@
+enum CoffeeType{
+    LATTE,
+    ESPRESSO
+}
+
+abstract class Coffee{
+    protected String name;
+
+    public String getName(){
+        return name;
+    }
+}
+
+class Latte extends Coffee{
+    public Latte(){
+        name = "latte";
+    }
+}
+
+class Espresso extends Coffee {
+    public Espresso(){
+        name = "Espresso";
+    }
+}
+
+class CoffeeFactory{
+    public static Coffee createCoffee(CoffeeType type){
+        switch(type){
+            case LATTE:
+                return new Latte();
+            case ESPRESSO:
+                return new Espresso();
+            default:
+                throw new IllegalArgumentException("Invaild coffee type: " + type);
+        }
+    }
+}
+
+public class FactoryPattern {
+    public static void main(String[] args) {
+        Coffee coffee = CoffeeFactory.createCoffee(CoffeeType.LATTE);
+        System.out.println(coffee.getName());
+    }
+}
