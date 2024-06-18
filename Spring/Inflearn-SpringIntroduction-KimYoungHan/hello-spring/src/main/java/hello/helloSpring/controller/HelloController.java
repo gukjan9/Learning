f24cdc0f962afe508e6d1d3cef41dde5e6d91382
GukJang <9ukjang@gmail.com>
2024-06-18 23:16:04 +0900

    api 방식

--- Spring/Inflearn-SpringIntroduction-KimYoungHan/hello-spring/src/main/java/hello/helloSpring/controller/HelloController.java
+++ Spring/Inflearn-SpringIntroduction-KimYoungHan/hello-spring/src/main/java/hello/helloSpring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,34 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // 소스 보기 해도 html 코드를 띄우지 않음
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;     // 데이터 그대로 내려줌
+    }
+
+    // 객체를 return
+    // json 으로 띄워줌
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
