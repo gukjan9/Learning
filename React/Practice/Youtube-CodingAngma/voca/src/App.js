f93cef67aaf1f286808390943f133dd6bdcf8f2f
GukJang <9ukjang@gmail.com>
2024-07-09 19:08:03 +0900

    DayList component

--- React/Practice/Youtube-CodingAngma/voca/src/App.js
+++ React/Practice/Youtube-CodingAngma/voca/src/App.js
@@ -1,9 +1,11 @@
+import DayList from "./component/DayList";
 import Header from "./component/Header";
 
 function App() {
   return (
   <div className="App">
     <Header />
+    <DayList />
   </div>
   );
 }
