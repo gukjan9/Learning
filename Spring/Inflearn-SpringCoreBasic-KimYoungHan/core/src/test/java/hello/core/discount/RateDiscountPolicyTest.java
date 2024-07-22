8e066a9fb6423c8dbc58edae5a116af9fc0c081c
GukJang <9ukjang@gmail.com>
2024-07-22 23:56:58 +0900

    VIP 가 아닐 때 테스트 코드

--- Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -23,4 +23,17 @@ class RateDiscountPolicyTest {
         // then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
+    void vip_x(){
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
 }
