9eb2f32f09346e261708dc9f57fca0bc7f7a3adb
GukJang <9ukjang@gmail.com>
2024-07-23 21:29:17 +0900

    관심사 분리로 DIP 를 지키기 위해 인터페이스에만 의존

--- Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,28 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // OCP 위반
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // DIP 를 지키고 있음
+    // 인터페이스에만 의존하고 구체적인 클래스에 대해선 모른다.
+    // (아래 discountPolicy 에 Fix 가 들어올지 Rate 가 들어올지 모름)
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 생성자
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
