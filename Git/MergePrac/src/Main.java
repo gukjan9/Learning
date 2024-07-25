ec4dc72fee9d9077387a49a07584a601d2d42b35
GukJang <9ukjang@gmail.com>
2024-07-25 23:33:00 +0900

    sout 2

--- Git/MergePrac/src/Main.java
+++ Git/MergePrac/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("b1 checkout");
+        System.out.println("b1 merge");
     }
 }
