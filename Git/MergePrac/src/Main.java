6c038daf682cecfbc79498ed65b072b644c85e19
GukJang <9ukjang@gmail.com>
2024-07-29 16:25:13 +0900

    init

--- Git/MergePrac/src/Main.java
+++ Git/MergePrac/src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("b1 checkout");
-        System.out.println("b1 merge");
-        System.out.println("git merge");
-
-        System.out.println("rebase");
-        System.out.println("main");
-
-        System.out.println("rebase");
-        System.out.println("b1");
     }
 }
