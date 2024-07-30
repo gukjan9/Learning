f3b3926ab9f4a696ed80c070922f44dd0792d206
GukJang <9ukjang@gmail.com>
2024-07-31 02:14:20 +0900

    스프링 컨테이너 활용

--- Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/main/java/hello/core/OrderApp.java
+++ Spring/Inflearn-SpringCoreBasic-KimYoungHan/core/src/main/java/hello/core/OrderApp.java
@@ -7,6 +7,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
@@ -15,9 +17,14 @@ public class OrderApp {
         // MemberService memberService = new MemberServiceImpl();
         // OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
