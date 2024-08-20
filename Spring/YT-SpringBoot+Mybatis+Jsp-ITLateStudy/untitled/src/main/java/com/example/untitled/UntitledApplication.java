b0752b94729842e402c25e4fb1ed59722f0a9eab
GukJang <9ukjang@gmail.com>
2024-08-20 10:45:49 +0900

    init

---
+++ Spring/YT-SpringBoot+Mybatis+Jsp-ITLateStudy/untitled/src/main/java/com/example/untitled/UntitledApplication.java
@@ -0,0 +1,13 @@
+package com.example.untitled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledApplication.class, args);
+	}
+
+}
