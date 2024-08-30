df40f97a6956732b0f3c56a4c17bda43fae7f86a
GukJang <9ukjang@gmail.com>
2024-08-30 09:55:58 +0900

    init

---
+++ Tools/Kafka/kafkaPrac1/src/main/java/com/prac/kafkaprac1/KafkaPrac1Application.java
@@ -0,0 +1,13 @@
+package com.prac.kafkaprac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPrac1Application.class, args);
+	}
+
+}
---
+++ Tools/Kafka/kafkaPrac1/src/test/java/com/prac/kafkaprac1/KafkaPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.kafkaprac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPrac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
