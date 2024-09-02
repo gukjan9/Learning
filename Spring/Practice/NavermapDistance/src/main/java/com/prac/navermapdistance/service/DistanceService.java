02dc07652f09721e9ac2fbad3e24d84833640da9
GukJang <9ukjang@gmail.com>
2024-09-02 17:56:56 +0900

    저장된 좌표를 이용한 distance 정보 구하기

--- Spring/Practice/NavermapDistance/src/main/java/com/prac/navermapdistance/service/DistanceService.java
+++ Spring/Practice/NavermapDistance/src/main/java/com/prac/navermapdistance/service/DistanceService.java
@@ -1,6 +1,5 @@
 package com.prac.navermapdistance.service;
 
-import com.prac.navermapdistance.config.NaverConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -14,10 +13,16 @@ import java.nio.charset.Charset;
 public class DistanceService {
 
     @Value("${naver.client.id}")
-    private String clientId;
+    private String naverClientId;
 
     @Value("${naver.client.secret}")
-    private String clientSecret;
+    private String naverClientSecret;
+
+    @Value("${ncp.client.id}")
+    private String ncpClientId;
+
+    @Value("${ncp.client.secret}")
+    private String ncpClientSecret;
 
     public ResponseEntity<String> searchPlace(String place){
 
@@ -33,8 +38,8 @@ public class DistanceService {
                 .toUri();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Naver-Client-Id", clientId);
-        headers.set("X-Naver-Client-Secret", clientSecret);
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClientSecret);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
@@ -47,9 +52,33 @@ public class DistanceService {
         return ResponseEntity.ok(responseBody);
     }
 
-    public ResponseEntity<String> distance(String departure, String arrival) {
+    public ResponseEntity<String> calcDistance(String departure, String arrival) {
 
-        return null;
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://naveropenapi.apigw.ntruss.com")
+                .path("/map-direction-15/v1/driving")
+                .queryParam("start", departure)
+                .queryParam("goal", arrival)
+                .queryParam("option", "trafast")
+                .queryParam("carType", 2)
+                .queryParam("fuelType", "gasoline")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-NCP-APIGW-API-KEY-ID", ncpClientId);
+        headers.set("X-NCP-APIGW-API-KEY", ncpClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+
+        String responseBody = responseEntity.getBody();
+
+        return ResponseEntity.ok(responseBody);
     }
 
 }
