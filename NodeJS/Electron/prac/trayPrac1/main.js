39c8728d73278cdd612b7e9f5c2dcdbda855b65a
GukJang <9ukjang@gmail.com>
2024-09-06 17:24:42 +0900

    메뉴 클릭시 웹페이지 콘솔로 로그 전송 (ipc)

--- NodeJS/Electron/prac/trayPrac1/main.js
+++ NodeJS/Electron/prac/trayPrac1/main.js
@@ -1,29 +1,59 @@
-const { app, BrowserWindow } = require('electron');
-const {Menu, Tray} = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, Tray } = require('electron');
+const path = require('path');
 
 let tray;
+let mainWindow;
+
 //트레이 아이콘
 function initTrayIconMenu(){
-  tray = new Tray('C:/Users/gukjang/GukJang/image/oasis-guitar.jpg'); 
+  tray = new Tray('C:/Users/gukjang/GukJang/image/star.png'); 
   const myMenu = Menu.buildFromTemplate([
-    {label: '1번', type: 'normal', checked: true, click: ()=>{console.log('1번클릭!')} },  //checked는 기본선택입니다.
-    {label: '2번', type: 'normal', click: ()=>{console.log('2번클릭!')}},
-    {label: '3번', type: 'normal', click: ()=>{console.log('3번클릭!')}}
+    {
+      label: '1번', type: 'normal', checked: true,
+      click: () => {
+        console.log('1번클릭!');
+        mainWindow.webContents.send('tray-click', '1번 클릭!');
+      },
+    },
+    {
+      label: '2번', type: 'normal',
+      click: () => {
+        console.log('2번클릭!');
+        mainWindow.webContents.send('tray-click', '2번 클릭!');
+      },
+    },
+    {
+      label: '3번', type: 'normal',
+      click: () => {
+        console.log('3번클릭!');
+        mainWindow.webContents.send('tray-click', '3번 클릭!');
+      },
+    },
   ])
   tray.setToolTip('트레이 아이콘!')
   tray.setContextMenu(myMenu)
 }
 
 // 브라우저 창을 생성하는 함수
-function createWindow () {  
-  let win = new BrowserWindow({
+function createWindow() {  
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true  //require같은 기능 사용 가능하도록
-    }
+      preload: path.join(__dirname, 'preload.js'), // preload.js를 사용하여 보안성을 높임
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
   });
-  win.loadFile('./html/index.html');
-  initTrayIconMenu();
+
+  mainWindow.loadFile('./html/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
-app.on('ready', createWindow);
+
+app.on('ready', () => {
+  createWindow();
+  initTrayIconMenu();
+});
